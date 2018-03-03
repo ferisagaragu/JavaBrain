e329b034e1abe17ec0ce01206e331a1939e20109
Fernando Isaías García Aguirre <figarcia@syesoftware.com>
2018-03-03 11:12:54 -0600

    SVG material desing

---
+++ javabrain-proyect/src/Main.java
@@ -0,0 +1,12 @@
+import brain.building.DNA;
+
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args){
+        JOptionPane.showMessageDialog(null,"Hola wee");
+        DNA dna = DNA.getInstance();
+    }
+
+}
