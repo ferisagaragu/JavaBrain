5fdc73bf8f199861b704d2e955d86669ecedae69
Fernando Isaías García Aguirre <figarcia@syesoftware.com>
2018-03-06 00:47:49 -0600

    Valuta -> Divisa

--- javabrain-traductor/src/com/google/traductor/Valuta.java
+++ javabrain-traductor/src/com/google/traductor/Valuta.java
@@ -13,30 +13,30 @@ import org.jsoup.select.Elements;
  *
  * @author Fernando García
  */
-public class Badge {
-	
+public class Valuta {
+
     private static Scraping s = new Scraping();
-    
+
     public static String mnxToUsdType(double mnx){
         DecimalFormat df = new DecimalFormat("#.0000 USD");
         double a = mnx / s.map.get("usd");
         return df.format(a);
     }
-    
+
     public static String usdToMnxType(double usd){
         DecimalFormat df = new DecimalFormat("#.0000 MNX");
         double a = usd * s.map.get("usd");
         return df.format(a);
     }
-    
+
     public static double mnxToUsd(double mnx){
         return mnx / s.map.get("usd");
     }
-    
+
     public static double usdToMnx(double usd){
         return usd * s.map.get("usd");
     }
-    
+
     public static void main(String[] args) {
         System.out.println(usdToMnxType(70.00));
         System.out.println(mnxToUsd(50.00));
@@ -44,15 +44,15 @@ public class Badge {
 }
 
 class Scraping{
-    
+
     public final String url = "https://www.hsbc.com.mx/1/2/es/personas/divisas";
     public final int maxPages = 2;
     public Map<String,Double> map = new HashMap();
-	
+
     public Scraping() {
-		
+
         for (int i=1; i<maxPages; i++){
-			
+
             String urlPage = String.format(url, i);
 
             if (getStatusConnectionCode(urlPage) == 200) {
@@ -72,28 +72,28 @@ class Scraping{
                 .replace(" ","")
                 .replace("$",",");
                 String[] divs = div.substring(1).split(",");
-                
+
                 for (int j = 0; j < divs.length; j++) {
                     String div1 = divs[j];
                     System.out.println(div1+" "+j);
                 }
-                
-                
+
+
                 map.put("usd",Double.parseDouble(divs[1]));
                 map.put("eur",Double.parseDouble(divs[3]));
                 map.put("gbp",Double.parseDouble(divs[5]));//Libra esterlina
-		
+
             }else{
                 System.out.println("El Status Code no es OK es: "+getStatusConnectionCode(urlPage));
                 break;
             }
         }
     }
-	
+
     public int getStatusConnectionCode(String url) {
-		
+
         Response response = null;
-		
+
         try {
             response = Jsoup.connect(url).userAgent("Mozilla/5.0").timeout(100000).ignoreHttpErrors(true).execute();
         } catch (IOException ex) {
@@ -101,7 +101,7 @@ class Scraping{
         }
         return response.statusCode();
     }
-	
+
     private Document getHtmlDocument(String url) {
 
         Document doc = null;
@@ -115,6 +115,5 @@ class Scraping{
         return doc;
 
     }
-    
-}
 
+}
