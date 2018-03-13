a580ba5e744f3ec7ed9fc3f044ce7641de06bbf3
Fernando Isaías García Aguirre <figarcia@syesoftware.com>
2018-03-13 08:31:40 -0600

    Arreglos performance
    -Agregación de cierres de buffers

--- javabrain-proyect/src/Main.java
+++ javabrain-proyect/src/Main.java
@@ -1,12 +1,16 @@
 import brain.building.DNA;
+import org.javabrain.util.web.service.Language;
+import org.javabrain.util.web.service.Traductor;
 
 import javax.swing.*;
 
 public class Main {
 
     public static void main(String[] args){
-        JOptionPane.showMessageDialog(null,"Hola wee");
-        DNA dna = DNA.getInstance();
+        /*JOptionPane.showMessageDialog(null,"Hola wee");
+        DNA dna = DNA.getInstance();*/
+        String data = Traductor.autoTraslate(Language.ENGLISH,"Oigan amigos, oigan amigos escuchenme a mi!!");
+        System.out.println(data);
     }
 
 }
--- javabrain-proyect/src/org/javabrain/util/web/service/Traductor.java
+++ javabrain-proyect/src/org/javabrain/util/web/service/Traductor.java
@@ -22,57 +22,63 @@ public class Traductor {
 
     private static ArrayList<Object> list = new ArrayList<>();
 
-    public static String traslate(Language origin,Language pastto,String text){
+    public static String traslate(Language origin, Language pastto, String text) {
         URL url;
         String linea = "";
         String newline = "";
+        BufferedReader in = null;
         try {
 
-            url = new URL("https://statickidz.com/scripts/traductor/?&source="+origin+"&target="+pastto.getType()+"&q="+URLEncoder.encode(text,"UTF-8"));
+            url = new URL("https://statickidz.com/scripts/traductor/?&source=" + origin + "&target=" + pastto.getType() + "&q=" + URLEncoder.encode(text, "UTF-8"));
             URLConnection con = url.openConnection();
-            BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            in = new BufferedReader(new InputStreamReader(con.getInputStream()));
 
             while ((linea = in.readLine()) != null) {
                 newline += linea;
             }
-            System.out.println(newline);
+
             JSONParser parser = new JSONParser();
             JSONObject obj = (JSONObject) parser.parse(newline);
             newline = obj.get("translation").toString();
-            
+
         } catch (Exception e) {
             System.out.println(e.getMessage());
+        }finally {
+            try{in.close();}catch (Exception e){}
         }
-        
+
         return newline;
     }
-    
-    //lista
-    public static String autoTraslate(Language pastto,String text){
+
+    public static String autoTraslate(Language pastto, String text) {
         URL url;
         String linea = "";
         String newline = "";
+        BufferedReader in = null;
         try {
 
-            url = new URL("https://statickidz.com/scripts/traductor/?&source=auto&target="+pastto.getType()+"&q="+URLEncoder.encode(text,"UTF-8"));
+            url = new URL("https://statickidz.com/scripts/traductor/?&source=auto&target=" + pastto.getType() + "&q=" + URLEncoder.encode(text, "UTF-8"));
             URLConnection con = url.openConnection();
-            BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            in = new BufferedReader(new InputStreamReader(con.getInputStream()));
 
             while ((linea = in.readLine()) != null) {
                 newline += linea;
             }
-            
+
             JSONParser parser = new JSONParser();
             JSONObject obj = (JSONObject) parser.parse(newline);
             newline = obj.get("translation").toString();
-            
+
         } catch (Exception e) {
             System.out.println(e.getMessage());
-        } 
-        
+        }finally {
+            try{in.close();}catch (Exception e){}
+        }
+
         return newline;
     }
 
+    //lista
     public static boolean addComponent(Object element){
         try {
             list.add(element);
